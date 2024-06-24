8e9be22770fc78bd27987ddc60cf57a36d9c8557
cuplprojects <shivom.chandrakala@gmail.com>
2024-06-24 11:01:03 +0530

    Alerts

--- src/pages/Imports/ScannedData.jsx
+++ src/pages/Imports/ScannedData.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
-const Scanned = ({ handleFileUpload, handleScannedUpload, selectedFile }) => {
+const Scanned = ({ handleFileUpload, handleScannedUpload, selectedFile,alertMessage,
+  alertType, loading}) => {
   return (
     <div className="tab-pane active" id="scanned">
       <h3 className="head text-center">Upload Scanned Data</h3>
@@ -9,10 +10,15 @@ const Scanned = ({ handleFileUpload, handleScannedUpload, selectedFile }) => {
           <input type="file" onChange={handleFileUpload} accept=".csv,.dat,.xlsx" />
         </p>
         {selectedFile && (
-          <button className="btn btn-primary" onClick={handleScannedUpload}>
-            Upload
+          <button className="btn btn-primary" onClick={handleScannedUpload} disabled={loading}>
+            {loading ? 'Uploading...' : 'Upload'}
           </button>
         )}
+        {alertMessage && (
+        <div className={`alert alert-${alertType} mt-3`} role="alert">
+          {alertMessage}
+        </div>
+      )}
       </div>
     </div>
   );
